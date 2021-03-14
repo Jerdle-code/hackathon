c73a2c8b1ebedbb3aa59c32c5dc6c8289f43416d
Lin314159 <danielamdurer@gmail.com>
2021-03-14 10:06:21 +0000

    Got the PHP working.

--- result.php
+++ result.php
@@ -2,13 +2,23 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Your Carbon Footprint</h1>
 
 <?php
-echo "<pre>";
-var_dump($_POST);
-var_dump(file_get_contents("php://input"));
-echo "</pre>";
+$co2_per_mile = array("smallpetrol"=>0.23877, "mediumpetrol"=>0.30029, "largepetrol"=>0.44752, "SUVpetrol"=>0.35156, "smalldiesel"=>0.22082, "mediumdiesel"=>0.26775, "largediesel"=>0.32863, "SUVdiesel"=>0.30805, "smallhybrid"=>0.16538, "mediumhybrid"=>0.17216, "largehybrid"=>0.23304, "SUVhybrid"=>0.31837, "smallelectric"=>0, "mediumelectric"=>0, "largeelectric"=>0, "SUVelectric"=>0);
+$car_type = $_POST["car-size"] . $_POST["car-fuel"];
+$car_co2 = $co2_per_mile[$car_type] * $_POST["miles"] * (365.2425/7);
+
+$co2_electric = $_POST["electric"] * (0.23314+0.02005) * (365.2425/7);
+$co2_gas = $_POST["gas"] * 2.02266 * 12;
+$co2_coal = $_POST["coal"] * 2883.26;
+$fuel_co2 = $co2_electric + $co2_gas + $co2_coal;
+
+$flight_co2 = $_POST["flights"]*1.6093844*0.19085*$_POST["flight-distance"];
+
+$total_co2 = $car_co2 + $fuel_co2 + $flight_co2;
+
+echo "Your carbon footprint is " . $total_co2 . "kg CO<sub>2</sub>e/yr."
 ?>
 
 </body>
